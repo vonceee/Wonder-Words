9653399496e28641f32ca6d86cd62c3e2a8987bd
YunahAFK <startingfromzero.04302025@gmail.com>
2025-12-12 06:43:59 +0800

    Fix Railway deployment configuration

--- database.php
+++ database.php
@@ -1,10 +1,12 @@
 <?php
-    $hostName = "localhost";
-    $dbUser = "root";
-    $dbPassword = "root";
-    $dbName = "sibucao_log_register";
+    // Get database config from environment or use local defaults
+    $hostName = getenv('DB_HOST') ?: "localhost";
+    $dbUser = getenv('DB_USER') ?: "root";
+    $dbPassword = getenv('DB_PASSWORD') ?: "root";
+    $dbName = getenv('DB_NAME') ?: "sibucao_log_register";
+    
     $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
     if (!$conn) {
-        die("Something went wrong!");
+        die("Database connection failed: " . mysqli_connect_error());
     }
 ?>
